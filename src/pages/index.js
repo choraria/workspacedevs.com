ae424b7649ac2aadaadb111d90e1c557fd8fe10e
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-01-05 17:39:20 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,29 @@ import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
 const IndexPage = ({data}) => {
-  console.log(data);
   return (
     <Layout>
-      <h1>Directory of Google Workspace Developers!</h1>
+      <div>
+        <h1>Directory of Google Workspace Developers!</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Location</th>
+              <th>Intro</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allMdx.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.frontmatter.name}</td>
+                <td>{node.frontmatter.location}</td>
+                <td>{node.frontmatter.intro}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   )
 }
