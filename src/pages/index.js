25aebe32b130ea6889a4472a650398d243994b85
Sourabh Choraria <sourabh.choraria@dashdash.com>
2021-01-05 17:29:10 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
+  console.log(data);
   return (
     <Layout>
       <h1>Directory of Google Workspace Developers!</h1>
     </Layout>
   )
 }
+
+export const query = graphql`
+query peopleQuery {
+  allMdx {
+    edges {
+      node {
+        frontmatter {
+          description
+          intro
+          location
+          name
+          slug
+          title
+        }
+        id
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
